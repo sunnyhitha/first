2bcf44af7f56f5c7e526ea45d88a2f6fe2d246d9
sunnyhitha <41725679+sunnyhitha@users.noreply.github.com>
2018-08-12 15:03:01 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+                     	String s;
+                        int b;
+                        Scanner sc=new Scanner(System.in);
+                        s=sc.next();
+                        b=sc.nextInt();
+                        char a[]=s.toCharArray();
+                        for(int i=0;i<b;i++)
+                        {
+                        System.out.print(a[i]);
+                        }	// your code goes here
+	}
+}
